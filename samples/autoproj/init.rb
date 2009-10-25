dd4d89ad403c9b3d60c135e423d0124597dd2874
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-10-25 09:20:30 +0100

    have bootstrap a complete autoproj/ template directory

--- samples/autoproj/init.rb
+++ samples/autoproj/init.rb
@@ -1,4 +1,17 @@
+# Write in this file customization code that will get executed before all the
+# soures are loaded.
+
 # Uncomment to initialize the environment variables to default values. This is
-# useful to ensure that the current shell environment will not mess up the build
-# Autoproj.set_initial_env
+# useful to ensure that the build is completely self-contained, but leads to
+# miss external dependencies installed in non-standard locations.
+#
+# set_initial_env
+#
+# Additionally, you can set up your own custom environment with calls to env_add
+# and env_set:
+#
+# env_add 'PATH', "/path/to/my/tool"
+# env_set 'VAR', "value"
+#
+# Variables set like this are exported in the generated env.sh scripts.
 
