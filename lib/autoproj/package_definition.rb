01f24be2cc827fcc22245a8560f05f36f4c313c0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-02-19 14:48:18 +0100

    documentation

--- lib/autoproj/package_definition.rb
+++ lib/autoproj/package_definition.rb
@@ -1,12 +1,31 @@
 module Autoproj
-    # Class used to store information about a package definition
+    # Autoproj-specific information about a package definition
+    #
+    # This stores the information that goes in addition to the autobuild package
+    # definitions
     class PackageDefinition
+        # @return [Autobuild::Package] the autobuild package definitins
         attr_reader :autobuild
+        # @return [Array<#call>] the set of blocks that should be called to
+        #   prepare the package. These are called before any operation has been
+        #   performed on the package iself.
         attr_reader :user_blocks
+        # @return [PackageSet] the package set that defined this package
         attr_reader :package_set
+        # @return [String] path to the file that contains this package's
+        #   definition
         attr_reader :file
+        # Whether this package is completely setup
+        #
+        # If the package is set up, its importer as well as all target
+        # directories are properly set, and all {user_blocks} have been called.
         def setup?; !!@setup end
+
+        # Sets the {setup?} flag
         attr_writer :setup
+
+        # @return [VCSDefinition] the version control information associated
+        #   with this package
         attr_accessor :vcs
 
         def initialize(autobuild, package_set, file)
@@ -15,10 +34,20 @@ module Autoproj
             @user_blocks = []
         end
 
+        # The package name
+        # @return [String]
         def name
             autobuild.name
         end
 
+        # Registers a setup block
+        #
+        # The block will be called when the setup phase is finished, or
+        # immediately if it is already finished (i.e. if {setup?} returns true)
+        #
+        # @param [#call] block the block that should be registered
+        # @yieldparam [Autobuild::Package] pkg the autobuild package object
+        # @see {user_blocks}
         def add_setup_block(block)
             user_blocks << block
             if setup?
