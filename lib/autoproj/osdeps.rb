ecd92f41cb9fa999c34e5dae33d414f5541bb9ad
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-05-27 09:42:18 +0200

    document ShellScriptManager

--- lib/autoproj/osdeps.rb
+++ lib/autoproj/osdeps.rb
@@ -6,7 +6,12 @@ module Autoproj
         # Base class for all package managers. Subclasses must add the
         # #install(packages) method and may add the
         # #filter_uptodate_packages(packages) method
+        #
+        # Package managers must be registered in PACKAGE_HANDLERS and
+        # (if applicable) OS_PACKAGE_HANDLERS.
         class Manager
+            # @return [Array<String>] the various names this package manager is
+            #   known about
             attr_reader :names
 
             attr_writer :enabled
@@ -15,12 +20,18 @@ module Autoproj
             attr_writer :silent
             def silent?; !!@silent end
 
+            # Create a package manager registered with various names
+            #
+            # @param [Array<String>] names the package manager names. It MUST be
+            #   different from the OS names that autoproj uses. See the comment
+            #   for OS_PACKAGE_HANDLERS for an explanation
             def initialize(names = [])
                 @names = names.dup
                 @enabled = true
                 @silent = true
             end
 
+            # The primary name for this package manager
             def name
                 names.first
             end
@@ -74,7 +85,7 @@ module Autoproj
         # Base class for all package managers that simply require the call of a
         # shell script to install packages (e.g. yum, apt, ...)
         class ShellScriptManager < Manager
-            def self.execute(script, with_locking,with_root)
+            def self.execute(script, with_locking, with_root)
                 if with_locking
                     File.open('/tmp/autoproj_osdeps_lock', 'w') do |lock_io|
                         begin
@@ -107,20 +118,78 @@ if test `id -u` != "0"; then
 fi
             EOSCRIPT
 
+            # Overrides the {#needs_locking?} flag
             attr_writer :needs_locking
-            attr_writer :needs_root
+            # Whether two autoproj instances can run this package manager at the
+            # same time
+            #
+            # This declares if this package manager cannot be used concurrently.
+            # If it is the case, autoproj will ensure that there is no two
+            # autoproj instances running this package manager at the same time
+            # 
+            # @return [Boolean]
+            # @see needs_locking=
             def needs_locking?; !!@needs_locking end
+
+            # Overrides the {#needs_root?} flag
+            attr_writer :needs_root
+            # Whether this package manager needs root access.
+            #
+            # This declares if the command line(s) for this package manager
+            # should be started as root. Root access is provided using sudo
+            # 
+            # @return [Boolean]
+            # @see needs_root=
             def needs_root?; !!@needs_root end
 
+            # Command line used by autoproj to install packages
+            #
+            # Since it is to be used for automated install by autoproj, it
+            # should not require any interaction with the user. When generating
+            # the command line, the %s slot is replaced by the quoted package
+            # name(s).
+            #
+            # @return [String] a command line pattern that allows to install
+            #   packages without user interaction. It is used when a package
+            #   should be installed by autoproj automatically
             attr_reader :auto_install_cmd
+            # Command line displayed to the user to install packages
+            #
+            # When generating the command line, the %s slot is replaced by the
+            # quoted package name(s).
+            #
+            # @return [String] a command line pattern that allows to install
+            #   packages with user interaction. It is displayed to the
+            #   user when it chose to not let autoproj install packages for this
+            #   package manager automatically
             attr_reader :user_install_cmd
 
+            # @param [Array<String>] names the package managers names, see
+            #   {#names}
+            # @param [Boolean] needs_locking whether this package manager can be
+            #   started by two separate autoproj instances at the same time. See
+            #   {#needs_locking?}
+            # @param [String] user_install_cmd the user-visible command line. See
+            #   {#user_install_cmd}
+            # @param [String] auto_install_cmd the command line used by autoproj
+            #   itself, see {#auto_install_cmd}.
+            # @param [Boolean] needs_root if the command lines should be started
+            #   as root or not. See {#needs_root?}
             def initialize(names, needs_locking, user_install_cmd, auto_install_cmd,needs_root=true)
                 super(names)
                 @needs_locking, @user_install_cmd, @auto_install_cmd,@needs_root =
                     needs_locking, user_install_cmd, auto_install_cmd, needs_root
             end
 
+            # Generate the shell script that would allow the user to install
+            # the given packages
+            #
+            # @param [Array<String>] os_packages the name of the packages to be
+            #   installed
+            # @option options [String] :user_install_cmd (#user_install_cmd) the
+            #   command-line pattern that should be used to generate the script.
+            #   If given, it overrides the default value stored in
+            #   {#user_install_cmd]
             def generate_user_os_script(os_packages, options = Hash.new)
                 user_install_cmd = options[:user_install_cmd] || self.user_install_cmd
                 if user_install_cmd
@@ -129,11 +198,28 @@ fi
                 end
             end
 
+            # Generate the shell script that should be executed by autoproj to
+            # install the given packages
+            #
+            # @param [Array<String>] os_packages the name of the packages to be
+            #   installed
+            # @option options [String] :auto_install_cmd (#auto_install_cmd) the
+            #   command-line pattern that should be used to generate the script.
+            #   If given, it overrides the default value stored in
+            #   {#auto_install_cmd]
             def generate_auto_os_script(os_packages, options = Hash.new)
                 auto_install_cmd = options[:auto_install_cmd] || self.auto_install_cmd
                 (auto_install_cmd % [os_packages.join("' '")])
             end
 
+            # Handles interaction with the user
+            #
+            # This method will verify whether the user required autoproj to
+            # install packages from this package manager automatically. It
+            # displays a relevant message if it is not the case.
+            #
+            # @return [Boolean] true if the packages should be installed
+            #   automatically, false otherwise
             def osdeps_interaction(os_packages, shell_script)
                 if OSDependencies.force_osdeps
                     return true
@@ -168,6 +254,17 @@ fi
                 false
             end
 
+            # Install packages using this package manager
+            #
+            # @param [Array<String>] packages the name of the packages that
+            #   should be installed
+            # @option options [String] :user_install_cmd (#user_install_cmd) the
+            #   command line that should be displayed to the user to install said
+            #   packages. See the option in {#generate_user_os_script}
+            # @option options [String] :auto_install_cmd (#auto_install_cmd) the
+            #   command line that should be used by autoproj to install said
+            #   packages. See the option in {#generate_auto_os_script}
+            # @return [Boolean] true if packages got installed, false otherwise
             def install(packages, options = Hash.new)
                 handled_os = OSDependencies.supported_operating_system?
                 if handled_os
@@ -202,7 +299,7 @@ fi
         # its package manager
         class HomebrewManager < ShellScriptManager
             def initialize
-                super(['brew'], true,
+                super(['homebrew'], true,
                         "brew install '%s'",
                         "brew install '%s'",
                         false)
@@ -799,12 +896,25 @@ fi
             PackageManagers::PortManager,
             PackageManagers::ZypperManager,
             PackageManagers::PipManager]
+        
+        # Mapping from OS name to package manager name
+        #
+        # Package handlers and OSes MUST have different names. The former are
+        # used to resolve packages and the latter to resolve OSes in the osdeps.
+        # Since one can force the use of a package manager in any OS by adding a
+        # package manager entry, as e.g.
+        #
+        # ubuntu:
+        #    homebrew: package
+        #
+        # we need to be able to separate between OS and package manager names.
         OS_PACKAGE_HANDLERS = {
             'debian' => 'apt-dpkg',
             'gentoo' => 'emerge',
             'arch' => 'pacman',
             'fedora' => 'yum',
-            'darwin' => 'port',
+            'macports' => 'port',
+            'brew' => 'homebrew',
             'opensuse' => 'zypper'
         }
 
