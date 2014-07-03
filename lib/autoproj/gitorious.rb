0dbb507d9a04f6e3b5b8a8d59d6194d8be15a805
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-07-03 21:01:52 +0200

    gitorious: make http,ssh the default access method for github
    It seems that github prefers that we use https as the pull access
    method, so do so. But for pushing, prefer ssh as it does not
    require a username and password.

--- lib/autoproj/gitorious.rb
+++ lib/autoproj/gitorious.rb
@@ -79,5 +79,5 @@ module Autoproj
 end
 
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
-Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com', :default => 'http,ssh')
 
