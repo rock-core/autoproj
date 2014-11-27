62ac199ba1c7a7d7f660d606a70dc768141dfe44
Sylvain Joyeux <sylvain.joyeux@fieb.org.br>
2014-11-27 13:15:31 -0200

    snapshot: move (most of) the implementation of autoproj-versions to autoproj/cli/versions
    This is to be in line with the ongoing cleanup work for autoproj 2.0.
    While the versioning work is too important to wait for 2.0, it would
    be good that it follows the new rules instead of continuing with the
    old ones ;-)

---
+++ lib/autoproj/cli/versions.rb
@@ -0,0 +1,93 @@
+require 'autoproj'
+require 'autoproj/cmdline'
+require 'autoproj/ops/tools'
+require 'autoproj/ops/snapshot'
+
+module Autoproj
+    module CLI
+        class Versions
+            include Ops::Tools
+
+            DEFAULT_VERSIONS_FILE_BASENAME = "50-versions.yml"
+
+            def default_versions_file
+                File.join( Autoproj.overrides_dir, DEFAULT_VERSIONS_FILE_BASENAME )
+            end
+
+            attr_reader :manifest
+
+            def initialize(manifest)
+                @manifest = manifest
+            end
+
+            def resolve_selection( user_selection )
+                resolved_selection = CmdLine.
+                    resolve_user_selection(user_selection, :filter => false)
+                resolved_selection.filter_excluded_and_ignored_packages(manifest)
+                # This calls #prepare, which is required to run build_packages
+                packages = CmdLine.import_packages(resolved_selection)
+
+                # Remove non-existing packages
+                packages.each do |pkg|
+                    if !File.directory?(manifest.package(pkg).autobuild.srcdir)
+                        raise ConfigError, "cannot commit #{pkg.name} as it is not checked out"
+                    end
+                end
+                packages
+            end
+
+
+            def parse_options(args)
+                options = Hash.new
+                parser = OptionParser.new do |opt|
+                    opt.on '--[no-]package-sets', 'commit the package set state as well (default if no packages are selected)' do |flag|
+                        options[:package_sets] = flag
+                    end
+                    opt.on '--replace', String, 'if the file given to --save exists, replace it instead of updating it' do
+                        options[:replace] = true
+                    end
+                    opt.on '-k', '--keep-going', "ignore packages that can't be snapshotted (the default is to terminate with an error)" do
+                        options[:keep_going] = true
+                    end
+                    opt.on '--save[=FILE]', String, "the file into which the versions should be saved (if no file is given, defaults to #{default_versions_file})" do |file|
+                        options[:output_file] =
+                            if file == '-'
+                                nil
+                            elsif !file
+                                default_output_file
+                            else
+                                file
+                            end
+                    end
+                end
+                common_options(parser)
+                remaining = parser.parse(args)
+                return remaining, options
+            end
+
+            def run(user_selection, options)
+                do_package_sets = options[:package_sets]
+                if do_package_sets.nil? && user_selection.empty?
+                    do_package_sets = true
+                end
+
+                CmdLine.report(silent: true) do
+                    packages = resolve_selection user_selection
+                    ops = Ops::Snapshot.new(manifest, keep_going: options[:keep_going])
+
+                    versions = Array.new
+                    if do_package_sets
+                        versions += ops.snapshot_package_sets
+                    end
+                    versions += ops.snapshot_packages(packages)
+                    if output_file = options[:output_file]
+                        ops.save_versions(versions, output_file, replace: options[:replace])
+                    else
+                        puts YAML.dump(versions)
+                    end
+                end
+            end
+        end
+    end
+end
+
