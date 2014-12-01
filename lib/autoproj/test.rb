5d24659dc0f956618ff7b6722a5c70cc36faa7fe
Sylvain Joyeux <sylvain.joyeux@fieb.org.br>
2014-12-01 17:39:24 -0200

    add common test setup file

---
+++ lib/autoproj/test.rb
@@ -0,0 +1,60 @@
+# simplecov must be loaded FIRST. Only the files required after it gets loaded
+# will be profiled !!!
+if ENV['TEST_ENABLE_COVERAGE'] == '1'
+    begin
+        require 'simplecov'
+        SimpleCov.start
+    rescue LoadError
+        require 'dummy_project'
+        Autoproj.warn "coverage is disabled because the 'simplecov' gem cannot be loaded"
+    rescue Exception => e
+        require 'dummy_project'
+        Autoproj.warn "coverage is disabled: #{e.message}"
+    end
+end
+
+require 'autoproj'
+## Uncomment this to enable flexmock
+require 'flexmock/test_unit'
+require 'minitest/spec'
+
+if ENV['TEST_ENABLE_PRY'] != '0'
+    begin
+        require 'pry'
+    rescue Exception
+        Autoproj.warn "debugging is disabled because the 'pry' gem cannot be loaded"
+    end
+end
+
+module Autoproj
+    # This module is the common setup for all tests
+    #
+    # It should be included in the toplevel describe blocks
+    #
+    # @example
+    #   require 'rubylib/test'
+    #   describe Autoproj do
+    #     include Autoproj::SelfTest
+    #   end
+    #
+    module SelfTest
+        if defined? FlexMock
+            include FlexMock::ArgumentTypes
+            include FlexMock::MockContainer
+        end
+
+        def setup
+            # Setup code for all the tests
+        end
+
+        def teardown
+            if defined? FlexMock
+                flexmock_teardown
+            end
+            super
+            # Teardown code for all the tests
+        end
+    end
+end
+
+
