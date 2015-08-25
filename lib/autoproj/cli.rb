3e854dfdc26782beda445e10f733ee5c243578bb
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-25 13:40:25 -0300

    support plugins using the gem-based mechanism

--- lib/autoproj/cli.rb
+++ lib/autoproj/cli.rb
@@ -3,6 +3,12 @@ module Autoproj
     module CLI
         class InvalidArguments < Autobuild::Exception
         end
+
+        def self.load_plugins
+            Gem.find_latest_files('autoproj-*', true).each do |path|
+                require path
+            end
+        end
     end
 end
 
