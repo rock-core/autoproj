20d8b8701ea017b0a1fe0a2fa2bc2c215869d950
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-25 13:40:37 -0300

    cli: fix osdeps initialization

--- lib/autoproj/cli/osdeps.rb
+++ lib/autoproj/cli/osdeps.rb
@@ -3,17 +3,6 @@ require 'autoproj/cli/inspection_tool'
 module Autoproj
     module CLI
         class OSDeps < InspectionTool
-            def validate_options(package_names, options = Hash.new)
-                package_names, options = super
-
-                initialize_and_load
-                if package_names.empty?
-                    package_names = ws.manifest.default_packages(false)
-                end
-
-                return package_names, options
-            end
-
             def run(user_selection, options = Hash.new)
                 initialize_and_load
                 _, osdep_packages, resolved_selection, _ =
