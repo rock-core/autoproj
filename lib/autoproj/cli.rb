b9c8308b07aa892c88ff9696116d291d91e2e901
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-10-20 16:35:35 -0200

    cli: be compatible with RubyGems shipped with ruby 2.0 when loading plugins

--- lib/autoproj/cli.rb
+++ lib/autoproj/cli.rb
@@ -4,7 +4,14 @@ module Autoproj
         end
 
         def self.load_plugins
-            Gem.find_latest_files('autoproj-*', true).each do |path|
+            finder_name =
+                if Gem.respond_to?(:find_latest_files)
+                    :find_latest_files
+                else
+                    :find_files
+                end
+
+            Gem.send(finder_name, 'autoproj-*', true).each do |path|
                 require path
             end
         end
