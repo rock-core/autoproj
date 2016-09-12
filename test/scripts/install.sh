59f01f63b732e80ac982224ff90f4d9d075ff44d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-09-12 16:21:36 -0300

    remove code coverage support in install tests
    It completely messes up the overall coverage.

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 set -ex
-if test "$TEST_ENABLE_COVERAGE" = "1"; then
-    $RUBY -I$PACKAGE_BASE_DIR/lib -e "require 'simplecov'; SimpleCov.start { command_name '$TEST_COMMAND_NAME'; root '$PACKAGE_BASE_DIR'; coverage_dir '$PACKAGE_BASE_DIR/coverage' }; load '$PACKAGE_BASE_DIR/bin/autoproj_install.in'" -- "$@"
-else
-    $RUBY -e "load \"$PACKAGE_BASE_DIR/bin/autoproj_install\"" -- "$@"
-fi
+$RUBY -e "load \"$PACKAGE_BASE_DIR/bin/autoproj_install\"" -- "$@"
 
 # vim: tw=0
