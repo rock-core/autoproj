5ecf1dc93db1151e29ccfc0bd3c0ffb2fd6782d8
Gabriel Arjones <arjones@arjones.com>
2018-10-26 14:10:22 -0300

    cli: test: fix linter offenses

--- lib/autoproj/cli/test.rb
+++ lib/autoproj/cli/test.rb
@@ -3,67 +3,69 @@ require 'autoproj/cli/inspection_tool'
 module Autoproj
     module CLI
         class Test < InspectionTool
-            def enable(user_selection, options = Hash.new)
+            def enable(user_selection, options = {})
                 if user_selection.empty?
                     ws.load_config
                     ws.config.utility_enable_all('test')
                 else
                     initialize_and_load
-                    selection, _ = finalize_setup(
+                    selection, = finalize_setup(
                         user_selection,
                         recursive: options[:deps],
-                        non_imported_packages: :return)
+                        non_imported_packages: :return
+                    )
                     ws.config.utility_enable('test', *selection)
                 end
                 ws.config.save
             end
 
-            def disable(user_selection, options = Hash.new)
+            def disable(user_selection, options = {})
                 if user_selection.empty?
                     ws.load_config
                     ws.config.utility_disable_all('test')
                 else
                     initialize_and_load
-                    selection, _ = finalize_setup(
+                    selection, = finalize_setup(
                         user_selection,
                         recursive: options[:deps],
-                        non_imported_packages: :return)
+                        non_imported_packages: :return
+                    )
                     ws.config.utility_disable('test', *selection)
                 end
                 ws.config.save
             end
 
-            def list(user_selection, options = Hash.new)
+            def list(user_selection, options = {})
                 initialize_and_load
-                resolved_selection, _ = finalize_setup(
+                resolved_selection, = finalize_setup(
                     user_selection,
                     recursive: options[:deps],
-                    non_imported_packages: :return)
+                    non_imported_packages: :return
+                )
 
-                lines = Array.new
+                lines = []
                 resolved_selection.each do |pkg_name|
                     pkg = ws.manifest.find_package_definition(pkg_name).autobuild
                     lines << [pkg.name, pkg.test_utility.enabled?, pkg.test_utility.available?]
                 end
                 lines = lines.sort_by { |name, _| name }
                 w     = lines.map { |name, _| name.length }.max
-                format = "%-#{w}s %-7s %-9s"
-                puts format % ["Package Name", "Enabled", "Available"]
+                out_format = "%-#{w}s %-7s %-9s"
+                puts format(out_format, 'Package Name', 'Enabled', 'Available')
                 lines.each do |name, enabled, available|
-                    puts(format % [name, (!!enabled).to_s, (!!available).to_s])
+                    puts(format(out_format, name, (!!enabled).to_s, (!!available).to_s))
                 end
             end
 
             def run(user_selection, deps: true)
                 initialize_and_load
-                packages, _ =
+                packages, =
                     finalize_setup(user_selection, recursive: deps)
                 packages.each do |pkg|
                     ws.manifest.find_autobuild_package(pkg).disable_phases('import', 'prepare', 'install')
                 end
-                Autobuild.apply(packages, "autoproj-test", ['test'])
+                Autobuild.apply(packages, 'autoproj-test', ['test'])
             end
         end
     end
 end
-
