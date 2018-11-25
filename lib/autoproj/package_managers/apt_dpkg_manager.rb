bf374677b31792ed6ad5fa7ce3cff1c91dfa67f6
Gabriel Arjones <arjones@arjones.com>
2018-11-24 23:28:28 -0300

    update debian os packages if a newer version is available

--- lib/autoproj/package_managers/apt_dpkg_manager.rb
+++ lib/autoproj/package_managers/apt_dpkg_manager.rb
@@ -8,15 +8,20 @@ module Autoproj
             def initialize(ws, status_file = "/var/lib/dpkg/status")
                 @status_file = status_file
                 @installed_packages = nil
+                @installed_versions = {}
                 super(ws, true,
                       %w{apt-get install},
                       %w{DEBIAN_FRONTEND=noninteractive apt-get install -y})
             end
 
-            def self.parse_package_status(installed_packages, paragraph)
+            def self.parse_package_status(installed_packages, paragraph, installed_versions: {})
                 if paragraph =~ /^Status: install ok installed$/
                     if paragraph =~ /^Package: (.*)$/
-                        installed_packages << $1
+                        package_name = $1
+                        installed_packages << package_name
+                        if paragraph =~ /^Version: (.*)$/
+                            installed_versions[package_name] = $1
+                        end
                     end
                     if paragraph =~ /^Provides: (.*)$/
                         installed_packages.merge($1.split(',').map(&:strip))
@@ -24,7 +29,7 @@ module Autoproj
                 end
             end
 
-            def self.parse_dpkg_status(status_file)
+            def self.parse_dpkg_status(status_file, installed_versions: {})
                 installed_packages = Set.new
                 dpkg_status = File.read(status_file)
                 dpkg_status << "\n"
@@ -36,17 +41,160 @@ module Autoproj
 
                 while paragraph_end = dpkg_status.scan_until(/Package: /)
                     paragraph = "Package: #{paragraph_end[0..-10]}"
-                    parse_package_status(installed_packages, paragraph)
+                    parse_package_status(installed_packages, paragraph, installed_versions: installed_versions)
                 end
                 parse_package_status(installed_packages, "Package: #{dpkg_status.rest}")
                 installed_packages
             end
 
+            def self.parse_apt_cache_paragraph(paragraph)
+                version = '0'
+                if paragraph =~ /^Package: (.*)$/
+                    package_name = $1
+                    if paragraph =~ /^Version: (.*)$/
+                        version = $1
+                    end
+                end
+                [package_name, version]
+            end
+
+            def self.parse_packages_versions(packages)
+                packages_versions = {}
+                apt_cache_show = `apt-cache show --no-all-versions #{packages.join(' ')}`
+                apt_cache_show = StringScanner.new(apt_cache_show)
+                if !apt_cache_show.scan(/Package: /)
+                    return packages_versions
+                end
+
+                while paragraph_end = apt_cache_show.scan_until(/Package: /)
+                    paragraph = "Package: #{paragraph_end[0..-10]}"
+                    package_name, version = AptDpkgManager.parse_apt_cache_paragraph(paragraph)
+                    packages_versions[package_name] = version
+                end
+                package_name, version = AptDpkgManager.parse_apt_cache_paragraph("Package: #{apt_cache_show.rest}")
+                packages_versions[package_name] = version
+                packages_versions
+            end
+
+            LESS = -1
+            EQUAL = 0
+            GREATER = 1
+
+            # Reference: https://www.debian.org/doc/debian-policy/ch-controlfields.html#version
+            def split_package_version(version)
+                epoch = '0'
+                debian_revision = '0'
+
+                upstream_version = version.split(':')
+                if upstream_version.size > 1
+                    epoch = upstream_version.first
+                    upstream_version = upstream_version[1..-1].join(':')
+                else
+                    upstream_version = upstream_version.first
+                end
+
+                upstream_version = upstream_version.split('-')
+                if upstream_version.size > 1
+                    debian_revision = upstream_version.last
+                    upstream_version = upstream_version[0..-2].join('-')
+                else
+                    upstream_version = upstream_version.first
+                end
+
+                [epoch, upstream_version, debian_revision]
+            end
+
+            def alpha?(lookAhead)
+                lookAhead =~ /[[:alpha:]]/
+            end
+
+            def digit?(lookAhead)
+                lookAhead =~ /[[:digit:]]/
+            end
+
+            def order(c)
+                if digit?(c)
+                    return 0
+                elsif alpha?(c)
+                    return c.ord
+                elsif c == '~'
+                    return -1
+                elsif c
+                    return c.ord + 256
+                else
+                    return 0
+                end
+            end
+
+            # Ported from https://github.com/Debian/apt/blob/master/apt-pkg/deb/debversion.cc
+            def compare_fragment(a, b)
+                i = 0
+                j = 0
+                while i != a.size && j != b.size
+                    first_diff = 0
+                    while i != a.size && j != b.size && (!digit?(a[i]) || !digit?(b[j]))
+                        vc = order(a[i])
+                        rc = order(b[j])
+                        return vc-rc if vc != rc
+                        i += 1
+                        j += 1
+                    end
+
+                    i += 1 while a[i] == '0'
+                    j += 1 while b[j] == '0'
+
+                    while digit?(a[i]) && digit?(b[j])
+                        first_diff = a[i].ord - b[j].ord if first_diff == 0
+                        i += 1
+                        j += 1
+                    end
+
+                    return 1 if digit?(a[i])
+                    return -1 if digit?(b[j])
+                    return first_diff if first_diff != 0
+                end
+
+                return 0 if i == a.size && j == b.size
+
+                if i == a.size
+                    return 1 if b[j] == '~'
+                    return -1
+                end
+
+                if j == b.size
+                    return -1 if a[i] == '~'
+                    return 1
+                end
+            end
+
+            def normalize_comparison_result(result)
+                return LESS if result < 0
+                return GREATER if result > 0
+                EQUAL
+            end
+
+            def compare_version(a, b)
+                a_split = split_package_version(a)
+                b_split = split_package_version(b)
+
+                [0, 1].each do |i|
+                    comp = normalize_comparison_result(compare_fragment(a_split[i], b_split[i]))
+                    return comp if comp != EQUAL
+                end
+                normalize_comparison_result(compare_fragment(a_split[2], b_split[2]))
+            end
+
+            def updated?(package, available_version)
+                # Consider up-to-date if the package is provided by another package (purely virtual)
+                # Ideally, we should check the version of the package that provides it
+                return true unless available_version && @installed_versions[package]
+                compare_version(available_version, @installed_versions[package]) != GREATER
+            end
+
             # On a dpkg-enabled system, checks if the provided package is installed
             # and returns true if it is the case
             def installed?(package_name, filter_uptodate_packages: false, install_only: false)
-                @installed_packages ||= AptDpkgManager.parse_dpkg_status(status_file)
-                
+                @installed_packages ||= AptDpkgManager.parse_dpkg_status(status_file, installed_versions: @installed_versions)
                 if package_name =~ /^(\w[a-z0-9+-.]+)/
                     @installed_packages.include?($1)
                 else
@@ -56,9 +204,10 @@ module Autoproj
             end
             
             def install(packages, filter_uptodate_packages: false, install_only: false)
+                packages_versions = AptDpkgManager.parse_packages_versions(packages)
                 if filter_uptodate_packages || install_only
                     packages = packages.find_all do |package_name|
-                        !installed?(package_name)
+                        !installed?(package_name) || !updated?(package_name, packages_versions[package_name])
                     end
                 end
 
--- test/package_managers/test_apt_dpkg_manager.rb
+++ test/package_managers/test_apt_dpkg_manager.rb
@@ -37,6 +37,85 @@ module Autoproj
                 mng = Autoproj::PackageManagers::AptDpkgManager.new(flexmock, file)
                 assert !mng.installed?('non-existent-package')
             end
+
+            LESS = Autoproj::PackageManagers::AptDpkgManager::LESS
+            EQUAL = Autoproj::PackageManagers::AptDpkgManager::EQUAL
+            GREATER = Autoproj::PackageManagers::AptDpkgManager::GREATER
+
+            def assert_version(a, result, b)
+                ws = flexmock
+                mng = Autoproj::PackageManagers::AptDpkgManager.new(ws)
+
+                assert_equal result, mng.compare_version(a, b)
+            end
+
+            # Tests extracted from https://github.com/Debian/apt/blob/master/test/libapt/compareversion_test.cc
+            def test_apt_package_version_comparison
+                assert_version '7.6p2-4', GREATER, '7.6-0'
+                assert_version '1.0.3-3', GREATER, '1.0-1'
+                assert_version '1.3', GREATER, '1.2.2-2'
+                assert_version '1.3', GREATER, '1.2.2'
+                assert_version '0-pre', EQUAL, '0-pre'
+                assert_version '0-pre', LESS, '0-pree'
+                assert_version '1.1.6r2-2', GREATER, '1.1.6r-1'
+                assert_version '2.6b2-1', GREATER, '2.6b-2'
+                assert_version '98.1p5-1', LESS, '98.1-pre2-b6-2'
+                assert_version '0.4a6-2', GREATER, '0.4-1'
+                assert_version '1:3.0.5-2', LESS, '1:3.0.5.1'
+                assert_version '1:0.4', GREATER, '10.3'
+                assert_version '1:1.25-4', LESS, '1:1.25-8'
+                assert_version '0:1.18.36', EQUAL, '1.18.36'
+                assert_version '1.18.36', GREATER, '1.18.35'
+                assert_version '0:1.18.36', GREATER, '1.18.35'
+                assert_version '9:1.18.36:5.4-20', LESS, '10:0.5.1-22'
+                assert_version '9:1.18.36:5.4-20', LESS, '9:1.18.36:5.5-1'
+                assert_version '9:1.18.36:5.4-20', LESS, ' 9:1.18.37:4.3-22'
+                assert_version '1.18.36-0.17.35-18', GREATER, '1.18.36-19'
+                assert_version '1:1.2.13-3', LESS, '1:1.2.13-3.1'
+                assert_version '2.0.7pre1-4', LESS, '2.0.7r-1'
+                assert_version '0:0-0-0', GREATER, '0-0'
+                assert_version '0', EQUAL, '0'
+                assert_version '0', EQUAL, '00'
+                assert_version '3.0~rc1-1', LESS, '3.0-1'
+                assert_version '1.0', EQUAL, '1.0-0'
+                assert_version '0.2', LESS, '1.0-0'
+                assert_version '1.0', LESS, '1.0-0+b1'
+                assert_version '1.0', GREATER, '1.0-0~'
+                assert_version '1.2.3', EQUAL, '1.2.3' # identical
+                assert_version '4.4.3-2', EQUAL, '4.4.3-2' # identical
+                assert_version '1:2ab:5', EQUAL, '1:2ab:5' # this is correct...
+                assert_version '7:1-a:b-5', EQUAL, '7:1-a:b-5' # and this
+                assert_version '57:1.2.3abYZ+~-4-5', EQUAL, '57:1.2.3abYZ+~-4-5' # and those too
+                assert_version '1.2.3', EQUAL, '0:1.2.3' # zero epoch
+                assert_version '1.2.3', EQUAL, '1.2.3-0' # zero revision
+                assert_version '009', EQUAL, '9' # zeroes…
+                assert_version '009ab5', EQUAL, '9ab5' # there as well
+                assert_version '1.2.3', LESS, '1.2.3-1' # added non-zero revision
+                assert_version '1.2.3', LESS, '1.2.4' # just bigger
+                assert_version '1.2.4', GREATER, '1.2.3' # order doesn't matter
+                assert_version '1.2.24', GREATER, '1.2.3' # bigger, eh?
+                assert_version '0.10.0', GREATER, '0.8.7' # bigger, eh?
+                assert_version '3.2', GREATER, '2.3' # major number rocks
+                assert_version '1.3.2a', GREATER, '1.3.2' # letters rock
+                assert_version '0.5.0~git', LESS, '0.5.0~git2' # numbers rock
+                assert_version '2a', LESS, '21' # but not in all places
+                assert_version '1.3.2a', LESS, '1.3.2b' # but there is another letter
+                assert_version '1:1.2.3', GREATER, '1.2.4' # epoch rocks
+                assert_version '1:1.2.3', LESS, '1:1.2.4' # bigger anyway
+                assert_version '1.2a+~bCd3', LESS, '1.2a++' # tilde doesn't rock
+                assert_version '1.2a+~bCd3', GREATER, '1.2a+~' # but first is longer!
+                assert_version '5:2', GREATER, '304-2' # epoch rocks
+                assert_version '5:2', LESS, '304:2' # so big epoch?
+                assert_version '25:2', GREATER, '3:2' # 25 > 3, obviously
+                assert_version '1:2:123', LESS, '1:12:3' # 12 > 2
+                assert_version '1.2-5', LESS, '1.2-3-5' # 1.2 < 1.2-3
+                assert_version '5.10.0', GREATER, '5.005' # preceding zeroes don't matters
+                assert_version '3a9.8', LESS, '3.10.2' # letters are before all letter symbols
+                assert_version '3a9.8', GREATER, '3~10' # but after the tilde
+                assert_version '1.4+OOo3.0.0~', LESS, '1.4+OOo3.0.0-4' # another tilde check
+                assert_version '2.4.7-1', LESS, '2.4.7-z' # revision comparing
+                assert_version '1.002-1+b2', GREATER, '1.00' # whatever...
+            end
         end
     end
 end
