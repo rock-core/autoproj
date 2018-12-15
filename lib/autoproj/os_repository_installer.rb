cd41c73c21d7281e35f78299480547e120bca28d
Gabriel Arjones <arjones@arjones.com>
2018-12-14 23:52:47 -0300

    implement OSRepositoryInstaller

---
+++ lib/autoproj/os_repository_installer.rb
@@ -0,0 +1,47 @@
+require 'autoproj/repository_managers/manager'
+require 'autoproj/repository_managers/unknown_os_manager'
+
+module Autoproj
+    class OSRepositoryInstaller
+        # The workspace object
+        attr_reader :ws
+
+        # Returns the set of repository managers
+        attr_reader :repository_managers
+
+        OS_REPOSITORY_MANAGERS = {
+        }.freeze
+
+        def initialize(ws)
+            @ws = ws
+            @repository_managers = {}
+            OS_REPOSITORY_MANAGERS.each do |name, klass|
+                @repository_managers[name] = klass.new(ws)
+            end
+        end
+
+        def os_repository_resolver
+            ws.os_repository_resolver
+        end
+
+        # Returns the repository manager object for the current OS
+        def os_repository_manager
+            return @os_repository_manager if @os_repository_manager
+
+            os_names, = os_repository_resolver.operating_system
+            os_name = os_names.find { |name| OS_REPOSITORY_MANAGERS[name] }
+
+            @os_repository_manager =
+                repository_managers[os_name] ||
+                RepositoryManagers::UnknownOSManager.new(ws)
+        end
+
+        def each_manager(&block)
+            repository_managers.each_value(&block)
+        end
+
+        def install_os_repositories
+            os_repository_manager.install(os_repository_resolver.resolved_entries)
+        end
+    end
+end
---
+++ lib/autoproj/repository_managers/manager.rb
@@ -0,0 +1,22 @@
+module Autoproj
+    module RepositoryManagers
+        # Base class for all repository managers. Subclasses must add the
+        # #install(entries) method
+        #
+        # Repository managers must be registered in OS_REPOSITORY_MANAGERS
+        class Manager
+            # @return [Workspace] the workspace
+            attr_reader :ws
+
+            # Create a repository manager
+            #
+            # @param [Workspace] ws the underlying workspace
+            def initialize(ws)
+                @ws = ws
+            end
+
+            def install(definitions)
+            end
+        end
+    end
+end
