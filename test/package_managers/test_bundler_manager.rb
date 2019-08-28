da816c1288bbb2c39b94bd08b0216679a3f49db6
Sylvain <sylvain.joyeux@tidewise.io>
2019-08-28 16:50:55 -0300

    bundler: allow setting build flags from Autoproj configuration
    Bundler allows configuring per-gem build flags, to let the gem
    that have C extensions find their dependencies.
    
    This commit adds the ability to set these flags within an
    Autoproj workspace, from e.g. within overrides.rb:
    
    ~~~
    Autoproj::PackageManagers::BundlerManager
    .configure_build_for(
      Autoproj.workspace,
      'gemname', '--some=other')
    ~~~

--- test/package_managers/test_bundler_manager.rb
+++ test/package_managers/test_bundler_manager.rb
@@ -15,6 +15,51 @@ module Autoproj
                                                gemfile: '/gem/path/Gemfile')
                 end
             end
+
+            describe "#initialize_environment" do
+                before do
+                    @ws = ws_create
+                    @bundler_manager = BundlerManager.new(@ws)
+                end
+
+                def run_bundler_config
+                    `BUNDLE_GEMFILE=#{@ws.prefix_dir}/gems/Gemfile bundle config`.split("\n")
+                end
+
+                it "adds build configurations" do
+                    BundlerManager.configure_build_for('testgem', '--some=config', ws: @ws)
+                    @bundler_manager.initialize_environment
+
+                    lines = run_bundler_config.each_cons(2).find do |a, b|
+                        a =~ /build.testgem/
+                    end
+                    assert_match(/Set for your local app.*: "--some=config"/,
+                                 lines[1])
+                end
+                it "updates existing build configurations" do
+                    BundlerManager.configure_build_for('testgem', '--some=config', ws: @ws)
+                    @bundler_manager.initialize_environment
+                    BundlerManager.configure_build_for('testgem', '--some=other', ws: @ws)
+                    @bundler_manager.initialize_environment
+
+                    lines = run_bundler_config.each_cons(2).find do |a, b|
+                        a =~ /build.testgem/
+                    end
+                    assert_match(/Set for your local app.*: "--some=other"/,
+                                 lines[1])
+                end
+                it "removes obsolete build configurations" do
+                    BundlerManager.configure_build_for('testgem', '--some=config', ws: @ws)
+                    @bundler_manager.initialize_environment
+                    BundlerManager.remove_build_configuration_for('testgem', ws: @ws)
+                    @bundler_manager.initialize_environment
+
+                    lines = run_bundler_config.each_cons(2).find do |a, b|
+                        a =~ /build.testgem/
+                    end
+                    refute lines
+                end
+            end
         end
     end
 end
