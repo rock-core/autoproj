a92bc9dd62b498d94731498e58dae9af454ea3ea
Sylvain <sylvain.joyeux@tidewise.io>
2019-09-02 20:39:45 -0300

    generate reports for utilities

--- test/cli/test_utility.rb
+++ test/cli/test_utility.rb
@@ -1,10 +1,10 @@
 require 'autoproj/test'
-require 'autoproj/cli/test'
+require 'autoproj/cli/utility'
 require 'timecop'
 
 module Autoproj
     module CLI
-        describe Test do
+        describe Utility do
             attr_reader :cli
             attr_reader :ws
             before do
@@ -13,8 +13,13 @@ module Autoproj
                 @two = ws_add_package_to_layout :cmake, 'two'
                 @three = ws_define_package :cmake, 'three'
 
+                @utility_class = Class.new(Autobuild::Utility)
+                Autobuild.register_utility_class(
+                    'unittest', @utility_class
+                )
+
                 @report_path = @ws.utility_report_path('unittest')
-                @cli = Test.new(ws, report_path: @report_path)
+                @cli = Utility.new(ws, name: 'unittest', report_path: @report_path)
                 flexmock(cli)
             end
 
@@ -39,42 +44,62 @@ module Autoproj
                         cli.run(%w[three])
                     end
                 end
+                it "generates a report if successful" do
+                    @cli.should_receive(:create_report)
+                        .with([@one, @two]).once
+                    cli.run(%w[one two])
+                end
+
+                it "creates a report on success" do
+                    @cli.should_receive(:create_report)
+                        .with([@one, @two]).once
+                    flexmock(Autobuild).should_receive(:apply)
+                    @cli.run(%w[one two])
+                end
+
+                it "creates a report on failure" do
+                    @cli.should_receive(:create_report)
+                        .with([@one, @two]).once
+                    flexmock(Autobuild).should_receive(:apply)
+                                       .and_raise(e = Class.new(Exception))
+                    assert_raises(e) { @cli.run(%w[one two]) }
+                end
+
+                it "does not create a report if the package resolution fails" do
+                    flexmock(@cli).should_receive(:create_report).never
+                    flexmock(@cli).should_receive(:finalize_setup)
+                                  .and_raise(e = Class.new(Exception))
+                    assert_raises(e) do
+                        @cli.run(%w[one two])
+                    end
+                end
             end
 
-            describe '#create_report' do
+            describe "#create_report" do
                 after do
                     Timecop.return
                 end
 
-                it 'reports the coverage status in addition to basic utility info' do
-                    a = @one.autobuild.test_utility
-                    a.source_dir = '/some/path'
-                    a.target_dir = '/some/other/path'
+                it "reports the status of the utility" do
+                    a = @one.autobuild.unittest_utility
+                    a.source_dir = "/some/path"
+                    a.target_dir = "/some/other/path"
                     a.task {}
-                    a.coverage_enabled = false
-                    a.coverage_source_dir = '/coverage/source/path'
-                    a.coverage_target_dir = nil
                     @cli.create_report([@one])
 
                     Timecop.freeze
                     report = JSON.load(File.read(@report_path))
-                    assert_equal Time.now.to_s, report['test_report']['timestamp']
-                    packages = report['test_report']['packages']
+                    assert_equal Time.now.to_s, report['unittest_report']['timestamp']
+                    packages = report['unittest_report']['packages']
                     assert_equal 1, packages.size
                     assert(one = packages['one'])
-                    assert one.key?('coverage_enabled')
                     assert_equal '/some/path', one['source_dir']
                     assert_equal '/some/other/path', one['target_dir']
                     assert_same true, one['available']
-                    assert_same false, one['enabled']
+                    assert_same true, one['enabled']
                     assert_same false, one['invoked']
                     assert_same false, one['success']
                     assert_same false, one['installed']
-
-                    assert_equal '/coverage/source/path', one['coverage_source_dir']
-                    assert_equal '/some/other/path/coverage', one['coverage_target_dir']
-                    assert_same true, one['coverage_available']
-                    assert_same false, one['coverage_enabled']
                 end
             end
         end
