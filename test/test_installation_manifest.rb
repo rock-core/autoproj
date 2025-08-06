8f9ca87fc04a368d258b76cc7b5425aaffd36ad3
Sylvain <sylvain.joyeux@tidewise.io>
2025-08-06 11:51:40 -0300

    feat: export package manifest information in the installation manifest

--- test/test_installation_manifest.rb
+++ test/test_installation_manifest.rb
@@ -11,6 +11,8 @@ module Autoproj
         it "registers a package" do
             pkg_def = ws_define_package :cmake, "test"
             flexmock(pkg_def.autobuild, dependencies: %w[a b c])
+
+            pkg_def.autobuild.description = package_manifest_fixture(pkg_def.autobuild)
             ws_define_package_vcs(
                 pkg_def, { type: "git", url: "https://github.com/some/repo" }
             )
@@ -27,6 +29,8 @@ module Autoproj
             assert_equal pkg.builddir, pkg_def.autobuild.builddir
             assert_equal pkg.logdir, pkg_def.autobuild.logdir
             assert_equal %w[a b c], pkg.dependencies
+
+            assert_package_manifest(pkg.manifest)
         end
 
         it "registers a package set" do
@@ -50,6 +54,7 @@ module Autoproj
                 pkg_def, { type: "git", url: "https://github.com/some/repo" }
             )
             ws_resolve_vcs(pkg_def)
+            pkg_def.autobuild.description = package_manifest_fixture(pkg_def.autobuild)
 
             options = {} # Workaround 2.6 brokenness
             ws_set = ws_define_package_set(
@@ -82,6 +87,75 @@ module Autoproj
             assert_equal pkg.builddir, pkg_def.autobuild.builddir
             assert_equal pkg.logdir, pkg_def.autobuild.logdir
             assert_equal %w[a b c], pkg.dependencies
+
+            assert_package_manifest(pkg.manifest)
+        end
+
+        def package_manifest_base_fields
+            {
+                description: "d",
+                brief_description: "brief_d",
+                tags: %w[some tags],
+                url: "u", license: "l", version: "v"
+            }
+        end
+
+        def assert_package_manifest(obj)
+            assert_kind_of InstallationManifest::Manifest, obj
+            package_manifest_base_fields.each do |key, value|
+                assert_equal value, obj.send(key)
+            end
+
+            assert_contact_info([%w[a_n1 a_e1], %w[a_n2 a_e2]], obj.authors)
+            assert_contact_info([%w[m_n1 m_e1], %w[m_n2 m_e2]], obj.maintainers)
+            assert_contact_info([%w[rm_n1 rm_e1], %w[rm_n2 rm_e2]], obj.rock_maintainers)
+            assert_dependencies(
+                [["p_n1", true, ["build"]], ["p_n2", false, ["runtime"]]],
+                obj.dependencies
+            )
+        end
+
+        def assert_dependencies(expected, actual)
+            expected.zip(actual) do |(name, optional, modes), obj|
+                assert_equal name, obj.name
+                if optional
+                    assert obj.optional
+                else
+                    refute obj.optional
+                end
+                assert_equal modes, obj.modes
+            end
+        end
+
+        def assert_contact_info(expected, actual)
+            assert_equal expected.size, actual.size
+            expected.zip(actual) do |(e_name, e_email), obj|
+                assert_equal e_name, obj.name
+                assert_equal e_email, obj.email
+            end
+        end
+
+        def package_manifest_fixture(autobuild)
+            pkg_manifest = PackageManifest.new(autobuild)
+            package_manifest_base_fields.each do |k, v|
+                pkg_manifest.send("#{k}=", v)
+            end
+            pkg_manifest.authors = make_contact_list(%w[a_n1 a_e1], %w[a_n2 a_e2])
+            pkg_manifest.maintainers = make_contact_list(%w[m_n1 m_e1], %w[m_n2 m_e2])
+            pkg_manifest.rock_maintainers =
+                make_contact_list(%w[rm_n1 rm_e1], %w[rm_n2 rm_e2])
+            pkg_manifest.dependencies.concat(
+                make_dependencies(["p_n1", true, ["build"]], ["p_n2", false, ["runtime"]])
+            )
+            pkg_manifest
+        end
+
+        def make_contact_list(*data)
+            data.map { |values| PackageManifest::ContactInfo.new(*values) }
+        end
+
+        def make_dependencies(*data)
+            data.map { |values| PackageManifest::Dependency.new(*values) }
         end
     end
 end
